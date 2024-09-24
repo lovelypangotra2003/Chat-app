33f49a99d91019523038c0f49104120f361d7555
lovelypangotra2003 <74448249+lovelypangotra2003@users.noreply.github.com>
2024-09-24 19:37:57 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home/Home';
 import SignUp from './pages/signup/SignUp';
-import Login from './pages/login/login';
+import Login from './pages/login/Login';
 import { Toaster } from 'react-hot-toast';
 import { useAuthContext } from './context/AuthContext';
 
